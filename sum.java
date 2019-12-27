e4c04e79ba627863049318e88c52d8c6a74a7c41
ThanishInfotech <thanishinfotech@gmail.com>
2019-12-27 12:24:00 +0530

    sum.java v2

--- sum.java
+++ sum.java
@@ -2,11 +2,11 @@ class sum
 {
 	public static void main(String [] args)
 	{
-		int ab,c;
+		int a,b,c;
 		a=60;
 		b=30;
 		c=30;
-		c=a+b+
+		c=a+b+c;
 		System.out.println("the Sum of three values "+c);
 	}
 }
